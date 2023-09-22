4d9d56312f5bf05300de6e8833719f31c774fea1
Shaleen Jain <shaleen@jain.sh>
2023-09-22 07:19:29 +0000

    add and use http-proxy

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import fetch, {Headers} from "node-fetch";
 import express from 'express'
+import proxy from 'express-http-proxy';
 
 const app = express()
 const port = 3000
@@ -8,10 +9,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/proxy/*', async (req, res) => {
+app.get('/v1/*', async (req, res) => {
     let url = req.url;
     console.log(url);
-    const prefix = '/proxy/'
+    const prefix = '/v1/'
 
     const remainingUrl = url.replace(new RegExp('^' + prefix), '')
     let targetUrl = decodeURIComponent(remainingUrl)
@@ -22,9 +23,9 @@ app.get('/proxy/*', async (req, res) => {
 
     try {
         const customHeaders = new Headers({
-            "Origin": "https://in.bookmyshow.com",
-            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/117.0",
-            "Referer": "https://in.bookmyshow.com",
+            "origin": "https://in.bookmyshow.com",
+            "user-agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/117.0",
+            "referer": "https://in.bookmyshow.com",
         });
         const result = await fetch(targetUrl, { method: "GET", headers: customHeaders}).then(res => res.text())
         return res.send(result)
@@ -34,6 +35,23 @@ app.get('/proxy/*', async (req, res) => {
         res.send('Bad Request')
     }
 })
+
+app.use('/v2', proxy("https://in.bookmyshow.com", {
+    userResHeaderDecorator(headers, userReq, userRes, proxyReq, proxyRes) {
+        const customHeaders = {
+            "origin": "https://in.bookmyshow.com",
+            "user-agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/117.0",
+            "referer": "https://in.bookmyshow.com",
+            "Connection": "keep-alive",
+            "Content-Type": "text/xml;charset=UTF-8",
+            "Accept": "*/*",
+            // ...headers
+        };
+        return customHeaders;
+    },
+    https: true,
+  })
+)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
